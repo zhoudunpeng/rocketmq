7b19940554a914a833951467f4a54ef7f3db2584
zhoudunpeng <zdpzxh@163.com>
2020-05-01 21:01:19 +0800

    事务消息

--- src/main/java/com/allen/rockettest/controller/TxController.java
+++ src/main/java/com/allen/rockettest/controller/TxController.java
@@ -30,7 +30,7 @@ public class TxController {
         TransactionSendResult sendResult = transactionProducer.getProducer().sendMessageInTransaction(message, otherParam);
 
         System.out.printf("发送结果 %s, msg=%s", sendResult.getSendStatus(), sendResult.toString()+"\n");
-        System.out.println(1231);
+        System.out.println(1123);
         return new HashMap<>();
     }
 }
